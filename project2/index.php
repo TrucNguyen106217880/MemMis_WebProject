7e7da56bd441df73a4f2c156a9b5598ae4fd2552
106217880 <106217880@student.swin.edu.au>
2025-10-29 11:03:18 +0700

    v1.3.0 PHP includes, basic settings.php set up

--- project2/index.php
+++ project2/index.php
@@ -13,29 +13,7 @@
 </head>
 
 <body>
-	<header>
-		<!-- This logo was created using a text-to-image AI tool provided by Canva, with modifications toward quality -->
-
-		<!-- CSS: Change size of this!!! -->
-		<img id="logo" src="images/MemMis_Logo.png"
-			 title="Custom Website Design & Development | MemMis"
-			 alt="MemMis logo" loading="lazy">
-		<nav>
-			<ul class="menu">
-				<li><a href="index.html" class="active">Home</a></li>
-				<li><a href="jobs.html">Job Positions</a>
-					<ul class="joblist">
-						<li><a href="jobs.html#SO145">Software Developer - SO145</a></li>
-						<li><a href="jobs.html#AI313">AI Engineer - AI313</a></li>
-						<li><a href="jobs.html#CY296">Cybersecurity Specialist - CY296</a></li>
-					</ul>
-				</li>
-				<li><a href="apply.html">Application</a></li>
-				<li><a href="about.html">About Us</a></li>
-				<li><a href="mailto:info@MemMis.com.au">Email: info@MemMis.com.au</a></li>
-			</ul>
-		</nav>
-	</header>
+	<header><?php include 'header.inc'; $current_page='index.php'; include 'menu.inc'; ?></header>
 
 	<main>
 		<!-- CSS: Call to action button, visual banner -->
@@ -68,7 +46,7 @@
 			<h3>Built by People Who Care</h3>
 			<p>MemMis was founded by a team who share a love for great design, clean code, and solving real problems.
 				We are agile, collaborative, and obsessed with quality.</p>
-			<a href="about.html">Know more about us!</a>
+			<a href="about.php">Know more about us!</a>
 		</section>
 
 		<section id="projects">
@@ -102,7 +80,7 @@
 			<p>We're growing — and we're looking for passionate developers, designers, and creative thinkers to join us.
 				If you're excited about building meaningful digital experiences, we'd love to hear from you.</p>
 			<!-- Call to action -->
-			<a href="jobs.html">See Open Positions</a>
+			<a href="jobs.php">See Open Positions</a>
 		</section>
 
 		<section id="contact">
@@ -112,17 +90,7 @@
 		</section>
 	</main>
 
-	<footer>
-		<p>&copy; 2025 MemMis. All rights reserved.</p>
-
-		<br>
-		<a href="https://trucnttkh.atlassian.net/jira/software/projects/MEMMIS/boards/34/backlog?atlOrigin=eyJpIjoiMzZiMzAwYmI1OTYyNDNjNDhlZDc2ZDk5OTI2NTFiYTgiLCJwIjoiaiJ9"
-		   target="_blank">Jira Project for MemMis website</a>
-
-		<br><br>
-		<a href="https://github.com/TrucNguyen106217880/MemMis_WebProject"
-		   target="_blank">GitHub Repository</a>
-	</footer>
+	<footer><?php include 'footer.inc'; ?></footer>
 </body>
 
 </html>
